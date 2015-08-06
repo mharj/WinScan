450564f632e0c0f12cddfe94709eb311775aeceb
Marko Harjula <marko.harjula@gmail.com>
2015-08-06 15:23:00 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,6 +10,47 @@ namespace WinScan
     {
         static void Main(string[] args)
         {
+            DirectoryLookup dl = new DirectoryLookup();
+            Stack<string> directoryList = new Stack<string>();   
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please give path to scan");
+                return;
+            }
+            string startDirectory = GetWindowsPhysicalPath(args[0]);
+            directoryList.Push(@startDirectory);
+            
+            while ( directoryList.Count != 0 )
+            {
+                string directory = directoryList.Pop();
+                try
+                {
+                    dl.SetDirectory(directory);
+                    FileData file;
+                    while ( (file = dl.GetNextFile()) != null )
+                    {
+                        if ( file.FileName.Equals(".snapshot") || file.FileName.Equals("..") || file.FileName.Equals("."))
+                        {
+                            // skip
+                        } 
+                        else
+                        { 
+                            if (file.IsDirectory == true)
+                            {
+                                directoryList.Push(directory + @"\" + file.FileName);
+                            }
+                            else
+                            {
+                                
+                            }
+                        }
+                    }
+                    dl.close();
+                } 
+                catch ( UnauthorizedAccessException ex)
+                {
+                    Console.WriteLine(ex.getMessage());
+                }
         }
     }
 }
