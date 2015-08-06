24fe6269fa0706bc67f7315270bcd9b29ed497e7
Marko Harjula <marko.harjula@gmail.com>
2015-08-06 15:25:44 +0300

    Update NativeMethods.cs

--- NativeMethods.cs
+++ NativeMethods.cs
@@ -15,5 +15,10 @@ namespace WinScan
         public static extern bool FindNextFile(IntPtr hFindFile, out WIN32_FIND_DATA lpFindFileData);
         [DllImport("kernel32", SetLastError = true, CharSet = CharSet.Unicode)]
         public static extern bool FindClose(IntPtr hFindFile);
+        
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern uint GetLongPathName(string ShortPath, StringBuilder sb, int buffer);
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        public static extern uint GetShortPathName(string longpath, StringBuilder sb, int buffer);
     }
 }
