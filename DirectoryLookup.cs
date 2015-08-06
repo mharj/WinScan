ce93c5e7195b7c31788dbf34197216ce4924fb80
Marko Harjula <marko.harjula@gmail.com>
2015-08-06 15:04:23 +0300

    Update DirectoryLookup.cs

--- DirectoryLookup.cs
+++ DirectoryLookup.cs
@@ -8,6 +8,83 @@ namespace WinScan
 {
     class DirectoryLookup
     {
+        public const int ERROR_SUCCESS = 0x0;
+        public const int ERROR_INVALID_FUNCTION = 0x1;
+        public const int ERROR_FILE_NOT_FOUND = 0x2;
+        public const int ERROR_PATH_NOT_FOUND = 0x3;
+        public const int ERROR_TOO_MANY_OPEN_FILES = 0x4;
+        public const int ERROR_ACCESS_DENIED = 0x5;
+        public const int ERROR_INVALID_NAME = 0x123;
+        bool disposed = false;
+        bool closed = false;
+        IntPtr h;
+        WIN32_FIND_DATA wfd = new WIN32_FIND_DATA();
+        private string dir;
+        private bool first = true;
+        IntPtr INVALID_HANDLE_VALUE = new IntPtr(-1);
 
+        public DirectoryLookup()
+        {
+            SetDirectory(dir);
+        }
+
+        ~DirectoryLookup()
+        {
+            close();
+        }
+
+        public void SetDirectory(string _dir)
+        {
+            dir = _dir;
+            first = true;
+        }
+
+        public FileData GetNextFile()
+        {
+            if (first == true)
+            {
+                first = false;
+                h = NativeMethods.FindFirstFile((@"\\?\" + dir + @"\*.*"), out wfd);
+                int error = Marshal.GetLastWin32Error();
+                if (h.Equals(INVALID_HANDLE_VALUE))
+                {
+                    switch (error)
+                    {
+                        case ERROR_ACCESS_DENIED: // soft exception
+                            throw new UnauthorizedAccessException("ERROR: Access denied " + dir);
+                        default:
+                            throw new Exception("Handle error [code:" + error + "] " + dir);
+                    }
+                }
+                closed = false;
+            }
+            else
+            {
+                if (!NativeMethods.FindNextFile(h, out wfd))
+                {
+                    return null;
+                }
+            }
+            return FileData.fromFindData(wfd);
+        }
+
+        public void close()
+        {
+            if ( closed == false )
+            {
+                NativeMethods.FindClose(h);
+                closed = true;
+                dir = null;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (disposed)
+            {
+                return;
+            }
+            close();
+        }
     }
 }
